eb2acfce19495820647117ff39e57528a46b72a0
Godwin Burume <godwinnyembo@gmail.com>
2024-01-11 20:50:42 +0100

    switch

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path');
 // Middleware pour les fichiers statiques
 app.use('/flags', express.static(path.join(__dirname, 'flags')));
 app.use('/logos', express.static(path.join(__dirname, 'logos')));
+app.use('/physicals', express.static(path.join(__dirname, 'physicals')));
 
 // Le reste de votre code de serveur...
 
